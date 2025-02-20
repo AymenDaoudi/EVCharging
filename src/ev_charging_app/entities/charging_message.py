b8474c484868681ad2bc324771fc0c570aa25f4b
Aymen Daoudi <iaymendaoudidev@gmail.com>
2025-02-19 22:15:17 -0500

    Updated charging event message + rename main app folder

--- src/ev_charging_app/entities/charging_message.py
+++ src/ev_charging_app/entities/charging_message.py
@@ -3,7 +3,11 @@ from typing import Any, Literal
 
 @dataclass(frozen=True)
 class ChargingMessage:
+    """
+    Represents a charging event message.
+    """
     event_type: Literal["start", "progress", "end"]
+    session_id: str
     station_id: str
     ev_id: str
     payload: dict[str, Any] 
